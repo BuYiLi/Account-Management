9cabd9f9a2c48e00d48395d76a202982235bf73d
limaoxu <1132989278@qq.com>
2018-10-08 22:09:30 +0800

    完成响应数据模板 部分环境搭建

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Account-Management
+
+#### 环境
+* jdk 1.8
+* maven 3.5.3
+* spring 5.1.0.RELEASE
+* url http://localhost:8080/account-management
+* tomcat 9.0.10
---
+++ src/main/java/com/account/management/common/entity/ResponseTemplateBuilder.java
@@ -0,0 +1,99 @@
+package com.account.management.common.entity;
+
+public class ResponseTemplateBuilder {
+
+    private int code;
+
+    private String msg;
+
+    private Object body;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public static class Build {
+        private ResponseTemplateBuilder builder;
+
+        public Build() {
+            this.builder = new ResponseTemplateBuilder();
+        }
+
+        public Build code(int code) {
+            builder.code = code;
+            return this;
+        }
+
+        public Build msg(String msg){
+            builder.msg = msg;
+            return this;
+        }
+
+        public Build body(Object body){
+            builder.body = body;
+            return this;
+        }
+
+        public ResponseTemplateBuilder build(){
+            return builder;
+        }
+    }
+
+    public static class Success{
+        private ResponseTemplateBuilder builder;
+
+        public Success(){
+            builder = new ResponseTemplateBuilder();
+            builder.code = 1;
+            builder.msg = "成功";
+        }
+
+        public Success body(Object body){
+            builder.body = body;
+            return this;
+        }
+
+        public ResponseTemplateBuilder build(){
+            return builder;
+        }
+    }
+
+    public static class Failed{
+        private ResponseTemplateBuilder builder;
+
+        public Success(){
+            builder = new ResponseTemplateBuilder();
+            builder.code = 1;
+            builder.msg = "成功";
+        }
+
+        public Success body(Object body){
+            builder.body = body;
+            return this;
+        }
+
+        public ResponseTemplateBuilder build(){
+            return builder;
+        }
+    }
+}
